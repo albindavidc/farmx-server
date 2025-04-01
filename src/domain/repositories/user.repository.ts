1d1f55817be1e33cafe15e1dce3d9acd3fe5f22a
Albin David C <albindavidc007@gmail.com>
2025-04-01 16:37:34 +0530

    updated user.repositories

---
+++ src/domain/repositories/user.repository.ts
@@ -0,0 +1,10 @@
+import { User } from "../entities/user.entity";
+import { Email } from "../value-objects/email.vo";
+
+export interface UserRepository {
+  create(user: User): Promise<User>;
+  findByEmail(email: Email): Promise<Email>;
+  findById(id: string): Promise<User | null>;
+  findByFirebaseUid(uid: string): Promise<User | null>;
+  update(user: User): Promise<User>;
+}
