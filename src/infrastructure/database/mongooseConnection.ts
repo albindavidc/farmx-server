523eb92f38da62b587696054f85217aa85bbf594
Albin David C <albindavidc007@gmail.com>
2025-04-10 10:34:04 +0530

    connected to the specific database in the atlas

--- src/infrastructure/database/mongooseConnection.ts
+++ src/infrastructure/database/mongooseConnection.ts
@@ -5,10 +5,12 @@ config();
 const connectToDatabase = async (): Promise<void> => {
   try {
     const mongoUri = process.env.MONGO_URI;
+    const dbName = process.env.MONGO_DB;
+
     if (!mongoUri) {
       throw new Error("Mongo Uri is not defined in the environment variables");
     }
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUri, {dbName});
     console.log("Your connected to the MongoDB");
   } catch (error) {
     console.error("Error while connecting to the MongoDB: ", error);
