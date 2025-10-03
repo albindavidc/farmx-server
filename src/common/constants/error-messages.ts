9c99031700487e62eee86b67819d3b7ec6d072da
Albin David C <albindavidc007@gmail.com>
2025-10-03 11:38:27 +0530

    created an seperated common constants folder

---
+++ src/common/constants/error-messages.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+  USER_NOT_FOUND: "User not found",
+  INVALID_EMAIL_OR_PASSWORD: "Invalid email or password",
+
+  ACCOUNT_NOT_VERIFIED: "Account not verified",
+  ACCOUNT_BLOCKED: "Account is blocked",
+};
